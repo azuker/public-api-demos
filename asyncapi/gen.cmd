f97c8d48e6722bee7b66918f2e76b78d2547d50d
Amir Zuker <amir.zuker@cyberproof.com>
2021-06-11 16:30:59 +0300

    update

---
+++ asyncapi/gen.cmd
@@ -0,0 +1,2 @@
+npx -p @asyncapi/generator ag --param server='production' asyncapi.json @asyncapi/nodejs-template o- ./nodejs-server
+npx -p @asyncapi/generator ag asyncapi.json @asyncapi/html-template -o ./docs
